67aeed8de20ae2f950f907439578c6ad48716d77
gurjarpravin4 <gurjarpravin4@gmail.com>
2023-12-25 18:21:35 +0530

    filtering and sorting by date done

--- src/components/expenses/ExpenseFilter.jsx
+++ src/components/expenses/ExpenseFilter.jsx
@@ -8,6 +8,8 @@ const ExpenseFilter = (props) => {
         <div className='flex justify-between items-center px-4 font-lato text-slate-600'>
             <label htmlFor="year">Select year:</label>
             <select className='w-28 h-8 rounded-md text-center focus:outline-none focus:border-2 focus:border-[#2a9df4]' name="year" value={props.selected} onChange={dropdownChangeHandler}>
+                <option value="all">All</option>
+                <option value="2023">2023</option>
                 <option value="2022">2022</option>
                 <option value="2021">2021</option>
                 <option value="2020">2020</option>
--- src/components/expenses/Expenses.jsx
+++ src/components/expenses/Expenses.jsx
@@ -9,12 +9,29 @@ const Expenses = (props) => {
     const filterChangeHandler = (selectedYear) => {
         setFilteredYear(selectedYear)
     }
+
+    let filteredExpenses
+    if(filteredYear === 'all') 
+        filteredExpenses = props.items
+    else{
+        filteredExpenses = props.items.filter((expense) => {
+            if (expense.date.getFullYear().toString() === filteredYear) {
+                return expense
+            }
+            return null
+        })
+    } 
+    
+    filteredExpenses.sort((a, b) => {
+        return b.date - a.date
+    })
+    
     return (
         <div>
             <Card className='bg-[#d0efff] max-w-5xl p-6 rounded-xl mx-auto'>
                 <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
                 {
-                    props.items.map((expense) => {
+                    filteredExpenses.map((expense) => {
                         return (
                             <ExpenseItem
                                 key={expense.id}
