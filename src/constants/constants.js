a6856058296cee4fc53c075c5dd4268743f41b28
gurjarpravin4 <gurjarpravin4@gmail.com>
2023-12-25 21:32:02 +0530

    final commit

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,26 +1,3 @@
 export const expenseList = [
-    {
-        id: '1',
-        date: new Date(2020, 7, 14),
-        title: 'Toilet Paper',
-        amount: 94.12
-    },
-    {
-        id: '2',
-        date: new Date(2021, 2, 12),
-        title: 'New TV',
-        amount: 799.49
-    },
-    {
-        id: '3',
-        date: new Date(2021, 2, 28),
-        title: 'Car Insurance',
-        amount: 294.67
-    },
-    {
-        id: '4',
-        date: new Date(2021, 5, 12),
-        title: 'New Desk (Wooden)',
-        amount: 450
-    }
+    
 ]
